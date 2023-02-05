39b074e397508cb7c5e332b9357490b10cb67225
GitHub Action <action@github.com>
2023-02-05 13:47:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/ohle/jythonsupport/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ohle/jythonsupport/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ohle.jythonsupport.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ohle.jythonsupport.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ohle/jythonsupport/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ohle/jythonsupport/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ohle.jythonsupport.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ohle.jythonsupport.MyBundle
 
 class MyProjectService(project: Project) {
 
